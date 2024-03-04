2529227eefa7f802bfd640d4b3981b081044cafd
Surya Pratap Singh <72143906+surya123-ctrl@users.noreply.github.com>
2024-03-04 23:37:39 +0530

    added useLogin hook

---
+++ src/hooks/useLogin.js
@@ -0,0 +1,28 @@
+import { useContext, useState } from "react";
+import axios from "axios";
+import { AuthContext } from "../context/AuthContext";
+import toast from "react-hot-toast";
+const useLogin = () => {
+    const [loading, setLoading] = useState(false);
+    const authUser = useContext(AuthContext)
+    const login = async (userDetails) => {
+        setLoading(true);
+        try {
+            const response = await axios.post(`http://localhost:8000/api/auth/login`, userDetails)
+            localStorage.setItem('chat-user', JSON.stringify(response.data));
+            authUser.setAuthUser(response.data)
+            console.log(response);
+            toast.success(response.data.message);
+        } catch (error) {
+            if (error.response && error.response.data && error.response.data.message) {
+                toast.error(error.response.data.message);
+            } else {
+                toast.error("An error occurred while signing up.");
+            }
+        } finally {
+            setLoading(false);
+        }
+    }
+    return [loading, login]
+}
+export default useLogin;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import useLogin from "../hooks/useLogin";
 const Login = () => {
+  const [loading, login] = useLogin();
+  const [userDetails, setUserDetails] = useState({
+    username: "",
+    password: "",
+  });
+  const handleChange = async (e) => {
+    const { name, value } = e.target;
+    setUserDetails((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await login(userDetails);
+  };
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
       <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
@@ -8,15 +25,20 @@ const Login = () => {
           Login
           <span className="text-pink-500"> ChatApp</span>
         </h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label className="label p-2">
               <span className="text-lg label-text text-gray-300">Username</span>
             </label>
             <input
               type="text"
+              name="username"
               placeholder="Enter Username"
               className="w-full input input-bordered h-10 text-black"
+              value={userDetails.username}
+              onChange={handleChange}
+              autoComplete="on"
+              required
             />
           </div>
           <div>
@@ -25,8 +47,13 @@ const Login = () => {
             </label>
             <input
               type="password"
+              name="password"
               placeholder="Enter Password"
               className="w-full input input-bordered h-10 text-black"
+              value={userDetails.password}
+              onChange={handleChange}
+              autoComplete="on"
+              required
             />
           </div>
           <Link
@@ -37,8 +64,15 @@ const Login = () => {
             {"Don't"} have an account?
           </Link>
           <div>
-            <button className="btn btn-block btn-sm mt-2 hover:bg-pink-500 hover:text-white">
-              Login
+            <button
+              className="btn btn-block btn-sm mt-2 hover:bg-pink-500 hover:text-white"
+              disabled={loading}
+            >
+              {loading ? (
+                <span className="loading loading-spinner"></span>
+              ) : (
+                "Login"
+              )}
             </button>
           </div>
         </form>
